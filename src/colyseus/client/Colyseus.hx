fbb032440bf09ce616c724cf5c67a147cf2e3a07
Sergey Balitskiy <sbalitskiy@gmail.com>
2019-04-25 12:57:24 +0200

    formatting, missing methods added

--- src/colyseus/client/Colyseus.hx
+++ src/colyseus/client/Colyseus.hx
@@ -1,33 +1,35 @@
 package colyseus.client;
 
-@:jsRequire("colyseus.js","Client")
+@:jsRequire("colyseus.js", "Client")
 extern class Client {
 	@:optional
-	var id: String;
-	var onOpen: Signal;
-	var onMessage: Signal;
-	var onClose: Signal;
-	var onError: Signal;
-	var protected: Dynamic;
-	var connection: Connection;
-	var rooms: Map<String,Room>;
-	var connectingRooms: Map<String,Room>;
-	var requestId: Float;
-	var hostname: String;
-	var storage: Storage;
-	var roomsAvailableRequests: Map<Int,Array<RoomAvailable>>;
+	var id:String;
+	var onOpen:Signal;
+	var onMessage:Signal;
+	var onClose:Signal;
+	var onError:Signal;
+	var protected:Dynamic;
+	var connection:Connection;
+	var rooms:Map<String, Room>;
+	var connectingRooms:Map<String, Room>;
+	var requestId:Float;
+	var hostname:String;
+	var storage:Storage;
+	var roomsAvailableRequests:Map<Int, Array<RoomAvailable>>;
 	function new(url:String, ?options:Dynamic):Void;
 	function join(roomName:String, ?options:Dynamic):Room;
-	function getAvailableRooms(roomName:String, callback:Array<RoomAvailable> -> ?String -> Void):Void;
+	function getAvailableRooms(roomName:String, callback:Array<RoomAvailable>->?String->Void):Void;
 	function close(colyseusId:String):Void;
 }
-@:jsRequire("colyseus.js","Connection")
+
+@:jsRequire("colyseus.js", "Connection")
 extern class Connection {
 	function new(url:Dynamic, ?querry:Dynamic):Void;
 	function onOpenCallback(even:Dynamic):Void;
 	function onCloseCallback(even:Dynamic):Void;
 	function send(data:Dynamic):Void;
 }
+
 @:enum abstract Protocol(Int) {
 	var USER_ID = 1;
 	var JOIN_ROOM = 10;
@@ -39,30 +41,33 @@ extern class Connection {
 	var ROOM_LIST = 20;
 	var BAD_REQUEST = 50;
 }
-@:jsRequire("colyseus.js","Signal")
+
+@:jsRequire("colyseus.js", "Signal")
 extern class Signal {
 	function new():Void;
 	function add(listener:haxe.Constraints.Function):Slot;
-    function addOnce(listener:haxe.Constraints.Function):Slot;
+	function addOnce(listener:haxe.Constraints.Function):Slot;
 }
-@:jsRequire("colyseus.js","Slot")
+
+@:jsRequire("colyseus.js", "Slot")
 extern class Slot {
 	function execute0():Void;
 	function execute1(value:Dynamic):Void;
 	function execute(valueObjects:Array<Dynamic>):Void;
-	var listener: haxe.Constraints.Function;
-	var readonly: Dynamic;
-	var once: Bool;
-	var priority: Float;
+	var listener:haxe.Constraints.Function;
+	var readonly:Dynamic;
+	var once:Bool;
+	var priority:Float;
 	function toString():String;
-	var enabled: Bool;
-	var params: Array<Dynamic>;
+	var enabled:Bool;
+	var params:Array<Dynamic>;
 	function remove():Void;
 	function verifyListener(listener:haxe.Constraints.Function):Void;
 }
-@:jsRequire("colyseus.js","StateContainer")
+
+@:jsRequire("colyseus.js", "StateContainer")
 extern class StateContainer {
-	var state: Dynamic;
+	var state:Dynamic;
 	function new(state:Dynamic):Void;
 	function set(newState:Dynamic):Void;
 	function registerPlaceholder(placeholder:String, matcher:js.RegExp):Void;
@@ -70,20 +75,21 @@ extern class StateContainer {
 	function removeListener(listener:Dynamic):Void;
 	function removeAllListeners():Void;
 }
-@:jsRequire("colyseus.js","Room")
+
+@:jsRequire("colyseus.js", "Room")
 extern class Room extends StateContainer {
-	var id: String;
-	var sessionId: String;
-	var name: String;
-	var options: Dynamic;
-	var clock: Clock;
-	var remoteClock: Clock;
-	var onJoin: Signal;
-	var onStateChange: Signal;
-	var onMessage: Signal;
-	var onError: Signal;
-	var onLeave: Signal;
-	var connection: Connection;
+	var id:String;
+	var sessionId:String;
+	var name:String;
+	var options:Dynamic;
+	var clock:Clock;
+	var remoteClock:Clock;
+	var onJoin:Signal;
+	var onStateChange:Signal;
+	var onMessage:Signal;
+	var onError:Signal;
+	var onLeave:Signal;
+	var connection:Connection;
 	function new(name:String, ?options:Dynamic):Void;
 	function connect(connection:Connection):Void;
 	function leave():Void;
@@ -92,11 +98,13 @@ extern class Room extends StateContainer {
 	function setState(encodedState:Dynamic, ?remoteCurrentTime:Float, ?remoteElapsedTime:Float):Void;
 	function patch(binaryPatch:Dynamic):Void;
 }
+
 typedef RoomAvailable = {
-	var roomId: String;
-	var clients: Float;
-	var maxClients: Float;
-	@:optional var metadata: Dynamic;
+	var roomId:String;
+	var clients:Float;
+	var maxClients:Float;
+	@:optional var metadata:Dynamic;
 };
+
 typedef Clock = Dynamic;
 typedef Storage = Dynamic;
--- src/colyseus/server/schema/Schema.hx
+++ src/colyseus/server/schema/Schema.hx
@@ -5,104 +5,113 @@ import haxe.macro.Context;
 import haxe.macro.Expr;
 
 typedef DecoratedField = {
-    field: Field,
-    meta: MetadataEntry
+	field:Field,
+	meta:MetadataEntry
 };
+
 #end
-@:jsRequire("@colyseus/schema","Schema")
+@:jsRequire("@colyseus/schema", "Schema")
 extern class Schema {}
 
-@:jsRequire("@colyseus/schema","ArraySchema")
+@:jsRequire("@colyseus/schema", "ArraySchema")
 extern class ArraySchema<T> {
-    public function new();
+	public function new();
 }
 
-
-@:jsRequire("@colyseus/schema","MapSchema")
+@:jsRequire("@colyseus/schema", "MapSchema")
 extern class MapSchema<T> {
-   public function new();
+	public function new();
 }
 
-//TODO: this is ugly way to get array acces working; perhaps could be a cleaner solution
+// TODO: this is ugly way to get array acces working; perhaps could be a cleaner solution
 class MapSchemaUtil {
-    #if !macro
-    public static function get<T>(m:MapSchema<T>, k:String):T return js.Syntax.code('{0}[{1}]',m,k);
-    public static function set<T>(m:MapSchema<T>, k:String, v:T):Void js.Syntax.code('{0}[{1}] = {2}',m,k,v);
-    #end
+	#if !macro
+	public static function get<T>(m:MapSchema<T>, k:String):T
+		return js.Syntax.code('{0}[{1}]', m, k);
+
+	public static function set<T>(m:MapSchema<T>, k:String, v:T):Void
+		js.Syntax.code('{0}[{1}] = {2}', m, k, v);
+
+	public static function delete<T>(m:MapSchema<T>, k:String)
+		return js.Syntax.code('delete {0}[{1}]', m, k);
+	#end
 }
 
 @:jsRequire("@colyseus/schema")
 extern class ExternDecorator {
-    public static function type(type:SchemaType):PropertyDecorator;
+	public static function type(type:SchemaType):PropertyDecorator;
 }
 
 typedef PropertyDecorator = Dynamic->String->Void;
 
 class Decorator {
-    #if macro
-    static public function build() {
-        var fields = Context.getBuildFields();
-        var decorated = getDecoratedFields(fields);
-        if (decorated.length > 0) emitInit(fields, decorated);
+	#if macro
+	static public function build() {
+		var fields = Context.getBuildFields();
+		var decorated = getDecoratedFields(fields);
+		if (decorated.length > 0)
+			emitInit(fields, decorated);
 
-        return fields;
-    }
-  
-    static function emitInit(fields: Array<Field>, decorated: Array<DecoratedField>)
-        fields.push({
-            name: '__init__',
-            access: [AStatic, APrivate],
-            kind: FFun({
-                    args: [],
-                    ret: macro :Void,
-                    expr: macro {
-                        $b{decorated.map(emitDecoration)}
-                    }
-                }),
-            pos: Context.currentPos()
-        });
-  
-    static function emitDecoration(params: DecoratedField) {
-        var localClass = Context.getLocalClass().get();
-        return macro ExternDecorator.type($e{params.meta.params[0]})(untyped ($i{localClass.name}).prototype, $v{params.field.name});
-    }
-  
-    static function getDecoratedFields(fields: Array<Field>)
-        return fields.map(getDecoration).filter(notNull);
-  
-    static function getDecoration(field: Field): DecoratedField {
-        for (meta in field.meta) {
-        if (meta.name == ':type') return {
-            field: field,
-            meta: meta
-        };
-        }
-        return null;
-    }
-  
-    static function notNull(v: Dynamic)
-        return v != null;
-    #end
+		return fields;
+	}
+
+	static function emitInit(fields:Array<Field>, decorated:Array<DecoratedField>)
+		fields.push({
+			name: '__init__',
+			access: [AStatic, APrivate],
+			kind: FFun({
+				args: [],
+				ret: macro:Void,
+				expr: macro {
+					$b{decorated.map(emitDecoration)}
+				}
+			}),
+			pos: Context.currentPos()
+		});
+
+	static function emitDecoration(params:DecoratedField) {
+		var localClass = Context.getLocalClass().get();
+		return macro ExternDecorator.type($e{params.meta.params[0]})(untyped ($i{localClass.name}).prototype, $v{params.field.name});
+	}
+
+	static function getDecoratedFields(fields:Array<Field>)
+		return fields.map(getDecoration).filter(notNull);
+
+	static function getDecoration(field:Field):DecoratedField {
+		for (meta in field.meta) {
+			if (meta.name == ':type')
+				return {
+					field: field,
+					meta: meta
+				};
+		}
+		return null;
+	}
+
+	static function notNull(v:Dynamic)
+		return v != null;
+	#end
 }
 
 @:autoBuild(colyseus.server.schema.Schema.Decorator.build())
 interface ISchema {}
 
-typedef SchemaType = Dynamic; 
-//TODO one of :TypePrimitive, Array<TypePrimitive>, {map:Dynamic})
+typedef SchemaType = Dynamic;
+
+// TODO one of :TypePrimitive, Array<TypePrimitive>, {map:Dynamic})
 
 @:enum abstract TypePrimitive(String) from String to String {
-    var STRING = "string"; 	    //utf8 strings	maximum byte size of 4294967295
-    var NUMBER = "number";	    //auto-detects the int or float type to be used. (adds an extra byte on output)	0 to 18446744073709551615
-    var BOOLEAN = "boolean";	//true or false	0 or 1
-    var INT8 = "int8";	        //signed 8-bit integer	-128 to 127
-    var UINT8 = "uint8";	    //unsigned 8-bit integer	0 to 255
-    var INT16 = "int16";	    //signed 16-bit integer	-32768 to 32767
-    var UINT16 = "uint16";	    //unsigned 16-bit integer	0 to 65535
-    var INT32 = "int32";	    //signed 32-bit integer	-2147483648 to 2147483647
-    var UINT32 = "uint32";	    //unsigned 32-bit integer	0 to 4294967295
-    var INT64 = "int64";	    //signed 64-bit integer	-9223372036854775808 to 9223372036854775807
-    var UINT64 = "uint64";	    //unsigned 64-bit integer	0 to 18446744073709551615
-    var FLOAT32 = "float32";	//single-precision floating-point number	-3.40282347e+38 to 3.40282347e+38
-    var FLOAT64 = "float64";	//double-precision floating-point number	-1.7976931348623157e+308 to 1.7976931348623157e+308
+	var STRING = "string"; // utf8 strings	maximum byte size of 4294967295
+	var NUMBER = "number"; // auto-detects the int or float type to be used. (adds an extra byte on output)	0 to 18446744073709551615
+	var BOOLEAN = "boolean"; // true or false	0 or 1
+	var INT8 = "int8"; // signed 8-bit integer	-128 to 127
+	var UINT8 = "uint8"; // unsigned 8-bit integer	0 to 255
+	var INT16 = "int16"; // signed 16-bit integer	-32768 to 32767
+	var UINT16 = "uint16"; // unsigned 16-bit integer	0 to 65535
+	var INT32 = "int32"; // signed 32-bit integer	-2147483648 to 2147483647
+	var UINT32 = "uint32"; // unsigned 32-bit integer	0 to 4294967295
+	var INT64 = "int64"; // signed 64-bit integer	-9223372036854775808 to 9223372036854775807
+	var UINT64 = "uint64"; // unsigned 64-bit integer	0 to 18446744073709551615
+	var FLOAT32 = "float32"; // single-precision floating-point number	-3.40282347e+38 to 3.40282347e+38
+	var FLOAT64 = "float64"; // double-precision floating-point number	-1.7976931348623157e+308 to 1.7976931348623157e+308
 }

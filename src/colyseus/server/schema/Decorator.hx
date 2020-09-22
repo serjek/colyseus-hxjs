2999c710e6835c205f689f89593a96537d7b4de1
Sergey Balitskiy <sergey.balitskiy@doclerholding.com>
2020-09-22 22:12:52 +0200

    added filter annotation, added map.clear() convenience method

---
+++ src/colyseus/server/schema/Decorator.hx
@@ -0,0 +1,54 @@
+package colyseus.server.schema;
+import haxe.macro.Context;
+import haxe.macro.Expr;
+
+using Lambda;
+
+typedef DecoratedField = {
+	field: Field,
+	meta: MetadataEntry
+};
+
+class Decorator {
+	static public function build() {
+		var fields = Context.getBuildFields();
+		var decorated = getDecoratedFields(fields);
+		if (decorated.length > 0)
+			emitInit(fields, decorated);
+
+		return fields;
+	}
+
+	static function emitInit(fields:Array<Field>, decorated:Array<DecoratedField>)
+		fields.push({
+			name: '__init__',
+			access: [AStatic, APrivate],
+			kind: FFun({
+				args: [],
+				ret: macro: Void,
+				expr: macro {
+					$b{decorated.map(emitDecoration)}
+				}
+			}),
+			pos: Context.currentPos()
+		});
+
+	static function emitDecoration(params:DecoratedField) {
+		var localClass = Context.getLocalClass().get();
+		return switch params.meta.name {
+			case ':type': macro ExternDecorator.type($e{params.meta.params[0]})(untyped ($i{localClass.name}).prototype, $v{params.field.name});
+			case ':filter': macro ExternDecorator.filter($e{params.meta.params[0]})(untyped ($i{localClass.name}).prototype, $v{params.field.name});
+			case _: null;
+		}
+	}
+
+	static function getDecoratedFields(fields:Array<Field>)
+		return fields.map(getDecoration).flatten();
+
+	static function getDecoration(field:Field):Array<DecoratedField>
+		return [
+			for (meta in field.meta)
+				if (meta.name == ':type' || meta.name == ':filter')
+					{field: field, meta: meta}
+		];
+}
--- src/colyseus/server/schema/Schema.hx
+++ src/colyseus/server/schema/Schema.hx
@@ -1,18 +1,12 @@
 package colyseus.server.schema;
-#if macro
-import haxe.macro.Context;
-import haxe.macro.Expr;
+import haxe.extern.EitherType;
+import colyseus.server.Client;
 
-typedef DecoratedField = {
-	field:Field,
-	meta:MetadataEntry
-};
-
-#end
 @:jsRequire("@colyseus/schema", "Schema")
-@:autoBuild(colyseus.server.schema.Schema.Decorator.build())
+@:autoBuild(colyseus.server.schema.Decorator.build())
 extern class Schema {}
 
+#if !macro
 @:jsRequire("@colyseus/schema", "ArraySchema")
 extern class ArraySchema<T> extends Array<T>{
 	public function new(rest:haxe.extern.Rest<T>);
@@ -23,88 +17,65 @@ private extern class MapSchemaImpl<T> extends haxe.ds.StringMap<T>{
 	public function new(?items:Any);
 }
 
-#if !macro
 @:forward
 abstract MapSchema<T>(MapSchemaImpl<T>) from MapSchemaImpl<T> to MapSchemaImpl<T> {
 	inline public function new(?items:Any) this = new MapSchemaImpl<T>(items);
 
-	inline public function set<T>(k:String, v:T):Void {
+	inline public function set<T>(k:String, v:T):Void
 		js.Syntax.code('{0}[{1}] = {2}', this, k, v);
-	}
 
 	inline public function get<T>(k:String):T
 		return js.Syntax.code('{0}[{1}]', this, k);
 
-	inline public function delete<T>(k:String):Void {
+	inline public function delete<T>(k:String):Void
 		js.Syntax.code('delete {0}[{1}]', this, k);
-	}
 
 	inline public function keys<T>():Array<String>
 		return js.Syntax.code('Object.keys({0})', this);
+
+	inline public function clear():Void
+		for (k in keys()) delete(k);
 }
 #end
 
 @:jsRequire("@colyseus/schema")
 extern class ExternDecorator {
 	public static function type(type:SchemaType):PropertyDecorator;
+	public static function filter(cb:FilterCallback):PropertyDecorator;
 }
 
 typedef PropertyDecorator = Dynamic->String->Void;
 
-class Decorator {
-	#if macro
-	static public function build() {
-		var fields = Context.getBuildFields();
-		var decorated = getDecoratedFields(fields);
-		if (decorated.length > 0)
-			emitInit(fields, decorated);
+/**
+ * see FilterCallback in @colyseus/schema/annotations.d.ts
+ * Usage in externs:
+ *
+	@:type(STRING)
+	@:filter(function(client, value, inst) {
+		trace(Reflect.fields(client)); //client fields, like id and sessionId
+		trace(Reflect.fields(value)); //current value of the field
+		trace(Reflect.fields(inst)); //instance if class where field is annotated
+		return true; //or false if value should be filtered
+	})
+	var someField:String;
+ */
 
-		return fields;
-	}
-
-	static function emitInit(fields:Array<Field>, decorated:Array<DecoratedField>)
-		fields.push({
-			name: '__init__',
-			access: [AStatic, APrivate],
-			kind: FFun({
-				args: [],
-				ret: macro:Void,
-				expr: macro {
-					$b{decorated.map(emitDecoration)}
-				}
-			}),
-			pos: Context.currentPos()
-		});
-
-	static function emitDecoration(params:DecoratedField) {
-		var localClass = Context.getLocalClass().get();
-		return macro ExternDecorator.type($e{params.meta.params[0]})(untyped ($i{localClass.name}).prototype, $v{params.field.name});
-	}
-
-	static function getDecoratedFields(fields:Array<Field>)
-		return fields.map(getDecoration).filter(notNull);
-
-	static function getDecoration(field:Field):DecoratedField {
-		for (meta in field.meta) {
-			if (meta.name == ':type')
-				return {
-					field: field,
-					meta: meta
-				};
-		}
-		return null;
-	}
-
-	static function notNull(v:Dynamic)
-		return v != null;
-	#end
-}
+typedef FilterCallback = Client -> Value -> Inst -> Bool;
+private typedef Inst = Dynamic;
+private typedef Value = Dynamic;
 
 typedef SchemaType = Dynamic;
 
-// TODO one of: TypePrimitive, [TypePrimitive], [Dynamic], {map:Dynamic}
+//TODO - strict type check in schema type:
+	/*EitherType<TypePrimitive,
+		EitherType<Array<TypePrimitive>,
+			EitherType<Array<Dynamic>,
+				EitherType<{map:TypePrimitive},{map:Dynamic}>
+			>
+		>
+	>;*/
 
-@:enum abstract TypePrimitive(String) from String to String {
+enum abstract TypePrimitive(String) to String {
 	var STRING = "string"; // utf8 strings	maximum byte size of 4294967295
 	var NUMBER = "number"; // auto-detects the int or float type to be used. (adds an extra byte on output)	0 to 18446744073709551615
 	var BOOLEAN = "boolean"; // true or false	0 or 1
